8ba03bb35d443078b6b23228f26ce371a7dd727c
zigzaggee <jw.ziggee@gmail.com>
2018-02-19 19:13:07 +0000

    lol
    loll

--- software/rpi/main.py
+++ software/rpi/main.py
@@ -133,10 +133,10 @@ class AssistantThread(Thread):
             status_ui.status('ready')
             self._can_start_conversation = True
             # Start the voicehat button trigger.
-            # aiy.voicehat.get_button().on_press(self._on_button_pressed)
-            self._snowboy = snowboythreaded.ThreadedDetector(self._on_detect, model, sensitivity=0.5)
-            self._snowboy.start()
-            self._snowboy.start_recog(sleep_time=0.03)
+            aiy.voicehat.get_button().on_press(self._on_button_pressed)
+            # self._snowboy = snowboythreaded.ThreadedDetector(self._on_detect, model, sensitivity=0.5)
+            # self._snowboy.start()
+            # self._snowboy.start_recog(sleep_time=0.03)
             if sys.stdout.isatty():
                 print('Say "Yogi", then speak. '
                       'Press Ctrl+C to quit...')
